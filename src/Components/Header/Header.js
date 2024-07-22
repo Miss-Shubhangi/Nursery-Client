6ef57b8775111dff76bea69172563853e93389bc
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-07-22 11:09:12 +0530

    header component implemented.

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'; // Import the CSS file
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <h1 className="nursery-name">Nursery Hub</h1>
+      <p className="tagline">Greenery for Every Space</p>
+    </header>
+  );
+};
+
+export default Header;
--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -2,23 +2,31 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import Plantcard from '../../Components/Plantcard/Plantcard'
+import "./Home.css"
+import Header from '../../Components/Header/Header'
 
 function Home() {
   const [plants , setPlants]=useState([])
 
   const loadPlants=async ()=>{
       const response=await axios.get(`http://localhost:8000/plants`)
-      console.log(response)
+      console.log(response.data.data)
+      setPlants(response.data.data)
   }
   useEffect(()=>{
     loadPlants()
   },[])
   return (
     <div>
-        <h1>Plants</h1>
+     <Header/>   
     {
-      Plants.map((plant,i)=>{
-        const {_id ,name ,category ,price , image,description}=plant
+      plants.map((plant,i)=>{
+        const {_id ,
+          name ,
+          category ,
+          price , 
+          image,
+          description}=plant
         return <Plantcard _id={_id}
         name={name}
         category={category}
